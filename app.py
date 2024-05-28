21932108896ba00eb767f1ebc9ea4e1ad85ed310
Igor <igor.propsht@gmail.com>
2024-05-27 21:46:57 -0400

    Section 2 Locators: xpath, others

--- app.py
+++ app.py
@@ -20,9 +20,6 @@ with sync_playwright() as playwright:
     # #Get the url
     # print("Docs:", page.url)
 
-
-
-
     # page.goto("https://bootswatch.com/default/")
     #
     # heading = page.get_by_role("heading", name="Heading 2")
@@ -35,11 +32,11 @@ with sync_playwright() as playwright:
     # page.goto("https://unsplash.com/")
     # page.get_by_alt_text("a green cloud floating over a lush green field").click()
 
-    #Title
+    # Title
     # page.goto("https://bootswatch.com/default/")
     # page.get_by_title("attribute").highlight()
 
-    #CSS
+    # CSS
     page.goto("https://bootswatch.com/default/")
     # page.locator("css=h1").highlight()
     # page.locator("footer").highlight()
@@ -62,9 +59,53 @@ with sync_playwright() as playwright:
     # page.locator("div.dropdown-menu:visible").highlight()
 
     # page.locator(":nth-match(button.btn-primary, 5)").highlight()
-    page.locator(":nth-match(button:text('Primary'),2)").highlight()
+    # page.locator(":nth-match(button:text('Primary'),2)").highlight()
 
+    # #XPATH
+    # page.locator("xpath=//h1[@id='navbars']").highlight()
+    # time.sleep(5)
+    # ## atribute
+    # page.locator("//input[@readonly]").highlight()
+    # time.sleep(5)
+    # ## value
+    # page.locator("//input[@value='wrong value']").highlight()
+    # time.sleep(5)
+    # ## text
+    # page.locator("xpath=//h1[ text() = 'Heading 1']").highlight()
+    # time.sleep(5)
+    # ## include text
+    # page.locator("xpath=//h1[ contains(text(), 'Head')]").highlight()
+    # time.sleep(5)
+    # page.locator("xpath=//button[contains(@class, 'btn-outline-primary') ]").highlight()
+    # time.sleep(5)
 
+    # page.locator("xpath=//input[ contains(@value, 'correct value')]").highlight()
+
+    # Other
+    # numeric element
+    # page.get_by_role("button", name="Primary").locator("nth=1").highlight()
+    # time.sleep(5)
+    # page.locator("button").locator("nth=18").highlight()
+
+    # Parent element
+    # page.get_by_label("Email address").highlight()
+    # time.sleep(5)
+    # page.get_by_label("Email address").locator("..").highlight()
+    # time.sleep(5)
+    #
+    #
+    # page.locator("id=btnGroupDrop1").highlight()
+    # time.sleep(5)
+    #
+    # page.locator("id=btnGroupDrop1").click()
+    # page.locator("div.dropdown-menu").locator("visible=true").highlight()
+    #
+    # # Element based on Filter
+    # page.get_by_role("heading").filter(has_text="Heading").highlight()
+
+    page.locator("div.bs-component").filter(
+        has=page.get_by_label("Password")
+    ).highlight()
 
     time.sleep(10)
     browser.close()
